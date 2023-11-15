17013c50c92d2061e0d07111c01b0526c1e72ab7
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-11-15 13:28:59 -0500

    added content for home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,36 @@
 // src/components/Home.js
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  font-size: 2.5rem;
+  color: ${({ theme }) => theme.palette.primary.main};
+  margin-bottom: 20px;
+`;
+
+const Introduction = styled.p`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.palette.text.secondary};
+`;
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome to the Photographer's Portfolio</h2>
-      {/* Add content specific to the Home section */}
-    </div>
+    <Container>
+      <Title>Welcome to the Photographer's Portfolio</Title>
+      <Introduction>
+        Explore the beauty captured through the lens. Our portfolio showcases
+        moments, emotions, and stories frozen in time. Every photograph has a
+        unique tale to tell. Let the images speak for themselves.
+      </Introduction>
+      {/* Add more content specific to the Home section */}
+    </Container>
   );
 };
 
