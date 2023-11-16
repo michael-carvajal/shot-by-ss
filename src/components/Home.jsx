b5cd26b06bde1931177fca247ffe248b4b3093b5
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-11-15 20:48:40 -0500

    added tailwindcss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,16 @@
 // src/components/Home.js
 import React from 'react';
-import styled from '@emotion/styled';
-
-const Container = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-  text-align: center;
-`;
-
-const Title = styled.h2`
-  font-size: 2.5rem;
-  color: ${({ theme }) => theme.palette.primary.main};
-  margin-bottom: 20px;
-`;
-
-const Introduction = styled.p`
-  font-size: 1.2rem;
-  color: ${({ theme }) => theme.palette.text.secondary};
-`;
 
 const Home = () => {
   return (
-    <Container>
-      <Title>Welcome to the Photographer's Portfolio</Title>
-      <Introduction>
-        Explore the beauty captured through the lens. Our portfolio showcases
-        moments, emotions, and stories frozen in time. Every photograph has a
-        unique tale to tell. Let the images speak for themselves.
-      </Introduction>
+    <div className="max-w-3xl mx-auto p-8 text-center">
+      <h2 className="text-3xl mb-4 text-primary">Welcome to the Photographer's Portfolio</h2>
+      <p className="text-base text-text-primary">
+        Explore the beauty captured through the lens. Our portfolio showcases moments, emotions, and stories frozen in time.
+        Every photograph has a unique tale to tell. Let the images speak for themselves.
+      </p>
       {/* Add more content specific to the Home section */}
-    </Container>
+    </div>
   );
 };
 
