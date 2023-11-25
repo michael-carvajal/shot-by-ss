5f1072428b9598a427e5a52a4e5038d47c8d7b78
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-11-25 12:00:39 -0500

    added images to gallery through the utis directory

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,36 @@
-// src/components/Gallery.js
 import React from 'react';
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import  {imagePaths}  from '../utils/importImages';
 const Gallery = () => {
+  // Example image paths
+
+
+
   return (
-    <div>
+    <div className="gallery-container">
       <h2>Gallery</h2>
-      {/* Add content specific to the Gallery section */}
+      <LazyLoadImage
+            src={''}
+            alt={`Image sdv`}
+            width="100%"
+            height="100%"
+            className="gallery-image"
+            effect="blur"
+          />
+      <div className="image-grid">
+        {imagePaths.map((path, index) => (
+          <LazyLoadImage
+            key={index}
+            src={path}
+            alt={`Image ${index + 1}`}
+            width="100%"
+            height="100%"
+            className="gallery-image"
+            effect="blur"
+          />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/utils/importImages.js
@@ -0,0 +1,69 @@
+import image_10 from   '../Images/instaImages/image_10.jpg'
+import image_19 from   '../Images/instaImages/image_19.jpg'
+import image_24 from   '../Images/instaImages/image_24.jpg'
+import image_29 from   '../Images/instaImages/image_29.jpg'
+import image_33 from   '../Images/instaImages/image_33.jpg'
+import image_39 from   '../Images/instaImages/image_39.jpg'
+import image_5 from   '../Images/instaImages/image_5.jpg'
+import image_12 from   '../Images/instaImages/image_12.jpg'
+import image_20 from   '../Images/instaImages/image_20.jpg'
+import image_25 from   '../Images/instaImages/image_25.jpg'
+import image_3 from   '../Images/instaImages/image_3.jpg'
+import image_34 from   '../Images/instaImages/image_34.jpg'
+import image_4 from   '../Images/instaImages/image_4.jpg'
+import image_6 from   '../Images/instaImages/image_6.jpg'
+import image_13 from   '../Images/instaImages/image_13.jpg'
+import image_26 from   '../Images/instaImages/image_26.jpg'
+import image_30 from   '../Images/instaImages/image_30.jpg'
+import image_35 from   '../Images/instaImages/image_35.jpg'
+import image_40 from   '../Images/instaImages/image_40.jpg'
+import image_8 from   '../Images/instaImages/image_8.jpg'
+import image_16 from   '../Images/instaImages/image_16.jpg'
+import image_22 from   '../Images/instaImages/image_22.jpg'
+import image_27 from   '../Images/instaImages/image_27.jpg'
+import image_31 from   '../Images/instaImages/image_31.jpg'
+import image_37 from   '../Images/instaImages/image_37.jpg'
+import image_43 from   '../Images/instaImages/image_43.jpg'
+import image_18 from   '../Images/instaImages/image_18.jpg'
+import image_23 from   '../Images/instaImages/image_23.jpg'
+import image_28 from   '../Images/instaImages/image_28.jpg'
+import image_32 from   '../Images/instaImages/image_32.jpg'
+import image_38 from   '../Images/instaImages/image_38.jpg'
+import image_44 from   '../Images/instaImages/image_44.jpg'
+
+const imagePaths = [
+    image_10,
+image_19,
+image_24,
+image_29,
+image_33,
+image_39,
+image_5,
+image_12,
+image_20,
+image_25,
+image_3,
+image_34,
+image_4,
+image_6,
+image_13,
+image_26,
+image_30,
+image_35,
+image_40,
+image_8,
+image_16,
+image_22,
+image_27,
+image_31,
+image_37,
+image_43,
+image_18,
+image_23,
+image_28,
+image_32,
+image_38,
+image_44
+]
+
+export { imagePaths}
